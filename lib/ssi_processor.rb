f0185001ecd58273e626e77374f4370e92d298fd
Thibaut Sacreste <thibaut.sacreste@gmail.com>
2012-08-07 12:55:30 +0100

    and once again forgot to git add

---
+++ lib/ssi_processor.rb
@@ -0,0 +1,67 @@
+module Rack
+  class SSIProcessor
+    
+    attr_accessor :logger, :locations
+    
+    def process(body)
+      # see http://wiki.nginx.org/HttpSsiModule
+      # currently only supporting 'block' and 'include' directives
+      blocks = {}
+      body.map do |part|
+        new_part = process_block(part) {|name, content| blocks[name] = content}
+        process_include(new_part, blocks)
+      end
+    end
+    
+    def process_block(part)
+      part.gsub(/<!--# block\s+name="(\w+)"\s+-->(.*?)<!--#\s+endblock\s+-->/) do
+        name, content = $1, $2
+        _info "processing block directive with name=#{name}"
+        yield [name, content]
+        ""
+      end
+    end
+    
+    def process_include(part, blocks)
+      part.gsub(/<!--#\s+include\s+(?:virtual|file)="([^"]+)"(?:\s+stub="(\w+)")?\s+-->/) do
+        location, stub = $1, $2
+        _info "processing include directive with location=#{location}"
+        status, _, body = fetch location
+        if stub && (status != 200 || body.nil? || body == "")
+          blocks[stub] 
+        else
+          body
+        end
+      end
+    end
+    
+    def fetch(location)
+      locations.select{|k,v| k.is_a?(String)}.each do |pattern, host|
+        return _get("#{host}#{location}") if location == pattern
+      end
+      locations.select{|k,v| k.is_a?(Regexp)}.each do |pattern, host|
+        return _get("#{host}#{location}") if location =~ pattern
+      end
+      _error "no match found for location=#{location}"
+    end
+    
+    private
+    
+    def _get(url)
+      _info "fetching #{url}"
+      RestClient.get(url) do |response, request, result|
+        _error "error fetching #{url}: #{response.code} response" if response.code != 200
+        [response.code, response.headers, response.body]
+      end
+    end
+    
+    def _info(message)
+      logger.info "Rack::SSI #{message}" if logger
+    end
+
+    def _error(message)
+      logger.info "Rack::SSI #{message}" if logger
+    end
+    
+  end
+end
---
+++ spec/ssi_processor_spec.rb
@@ -0,0 +1,195 @@
+describe Rack::SSIProcessor do
+  
+  describe "#process_block" do
+    it "should yield block directives and strip them out of the html" do
+      html = <<-eos
+        <html>
+          <body>
+            <!--# block name="shush" --><!--# endblock -->
+            <p>some content</p>
+            <!--# block name="shouty" --><h1>ERROR!</h1><!--# endblock -->
+          </body>
+        </html>
+      eos
+      
+      expected = <<-eos.gsub /\s+/, ""
+        <html>
+          <body>
+            <p>some content</p>
+          </body>
+        </html>
+      eos
+     
+      ssi = Rack::SSIProcessor.new
+      blocks = []
+
+      processed = ssi.process_block(html) {|block| blocks << block}
+
+      processed.gsub(/\s+/, "").should == expected
+      blocks.should == [["shush", ""], ["shouty", "<h1>ERROR!</h1>"]]      
+    end
+  end
+  
+  describe "#process_include" do
+    context "the SSI include request returns a valid response" do
+      it "should replace include directives with appropriate content" do
+        html = <<-eos
+          <html>
+            <body>
+              <!--# include virtual="/some/location" -->
+              <!--# include virtual="/some/other/location" -->
+            </body>
+          </html>
+        eos
+      
+        expected = <<-eos.gsub /\s+/, ""
+          <html>
+            <body>
+              <p>some content</p>
+              <p>some more content</p>
+            </body>
+          </html>
+        eos
+     
+        ssi = Rack::SSIProcessor.new
+        ssi.stub(:fetch).with("/some/location").and_return([200, {}, "<p>some content</p>"])
+        ssi.stub(:fetch).with("/some/other/location").and_return([200, {}, "<p>some more content</p>"])
+
+        processed = ssi.process_include(html, {})
+
+        processed.gsub(/\s+/, "").should == expected
+      end
+    end
+    
+    context "the SSI include request returns an empty response" do
+      it "should replace include directives with the content of the block specified by the 'stub' parameter" do
+        html = <<-eos
+          <html>
+            <body>
+              <!--# include virtual="/some/broken/location" stub="oops" -->
+            </body>
+          </html>
+        eos
+      
+        expected = <<-eos.gsub /\s+/, ""
+          <html>
+            <body>
+              <p>oops, something went wrong!</p>
+            </body>
+          </html>
+        eos
+     
+        ssi = Rack::SSIProcessor.new
+        ssi.stub(:fetch).with("/some/broken/location").and_return([200, {}, ""])
+
+        processed = ssi.process_include(html, {"oops" => "<p>oops, something went wrong!</p>"})
+
+        processed.gsub(/\s+/, "").should == expected
+      end
+      
+      it "should replace include directives with the empty response if no 'stub' parameter" do
+        html = <<-eos
+          <html>
+            <body>
+              <!--# include virtual="/some/broken/location" -->
+            </body>
+          </html>
+        eos
+        
+        ssi = Rack::SSIProcessor.new
+        ssi.stub(:fetch).with("/some/broken/location").and_return([200, {}, ""])
+
+        processed = ssi.process_include(html, {})
+
+        processed.gsub(/\s+/, "").should == "<html><body></body></html>"
+      end
+    end
+    
+    context "the SSI include request returns an error response" do
+      it "should replace include directives with the content of the block specified by the 'stub' parameter" do
+        html = <<-eos
+          <html>
+            <body>
+              <!--# include virtual="/some/broken/location" stub="oops" -->
+            </body>
+          </html>
+        eos
+      
+        expected = <<-eos.gsub /\s+/, ""
+          <html>
+            <body>
+              <p>oops, something went wrong!</p>
+            </body>
+          </html>
+        eos
+     
+        ssi = Rack::SSIProcessor.new
+        ssi.stub(:fetch).with("/some/broken/location").and_return([500, {}, "<crap>"])
+
+        processed = ssi.process_include(html, {"oops" => "<p>oops, something went wrong!</p>"})
+
+        processed.gsub(/\s+/, "").should == expected
+      end
+      
+      it "should replace include directives with the error response if no 'stub' parameter" do
+        html = <<-eos
+          <html>
+            <body>
+              <!--# include virtual="/some/broken/location" -->
+            </body>
+          </html>
+        eos
+        
+        ssi = Rack::SSIProcessor.new
+        ssi.stub(:fetch).with("/some/broken/location").and_return([500, {}, "<bang>"])
+
+        processed = ssi.process_include(html, {})
+
+        processed.gsub(/\s+/, "").should == "<html><body><bang></body></html>"
+      end
+    end
+    
+  end
+  
+  describe "#fetch" do
+    it "should resolve locations by exact match first" do
+      ssi = Rack::SSIProcessor.new
+      ssi.locations = {
+        /\/pants/ => "http://host1",
+        "/pants" => "http://host2"
+      }
+      
+      RestClient.should_receive(:get).with("http://host2/pants")
+      ssi.fetch("/pants")      
+    end
+    
+    it "should resolve locations by regex if no exact match" do
+      ssi = Rack::SSIProcessor.new
+      ssi.locations = {
+        /^\/pants\/.*/ => "http://host1",
+        "/pants" => "http://host2"
+      }
+      
+      RestClient.should_receive(:get).with("http://host1/pants/on/fire")
+      ssi.fetch("/pants/on/fire")      
+    end
+  end
+  
+  describe "#process" do
+    it "should do it all!" do
+      body = [
+        '<html><body>',
+        '<!--# block name="shouty" --><p>ERROR!</p><!--# endblock -->',
+        '<!--# include virtual="/includes/broken" stub="shouty" -->',
+        '<!--# include virtual="/includes/header" -->',
+        '</body></html>'
+      ]
+      ssi = Rack::SSIProcessor.new
+      ssi.stub(:fetch).with("/includes/broken").and_return([500, {}, "<p>pants!</p>"])
+      ssi.stub(:fetch).with("/includes/header").and_return([200, {}, "<h1>Hello</h1>"])
+      
+      ssi.process(body).join.should == "<html><body><p>ERROR!</p><h1>Hello</h1></body></html>"
+    end
+  end
+  
+end
